cba115097b4bda2a06cea0a1153c57e8a5b61364
Pranjal Srivastava <5683pranjal@gmail.com>
2025-05-05 20:57:50 +0530

    Added backend_sanity files

---
+++ backend_sanity/schemaTypes/index.ts
@@ -0,0 +1,19 @@
+import testimonials from "./testimonials"
+import about from "./about"
+import brand from "./brand"
+import contact from "./contact"
+import experience from "./experience"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+
+export const schemaTypes = [
+    about,
+    brand,
+    contact,
+    experience,
+    skills,
+    workExperience,
+    works,
+    testimonials,
+]
